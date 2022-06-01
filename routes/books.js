200b1c0d8e5d0ac5c3c89d5f3bd92bc36733ebfd
sushu2001 <sushantawathare212@gmail.com>
2022-06-02 00:46:12 +0530

    Done Satisfying changes upto 6thvideo

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,8 @@
 const express = require('express')
-const Author = require('../models/author')
 const router = express.Router()
 const Book = require('../models/book')
-const imageMimeTypes = ['image/jpeg', 'image/gif', 'image/png']
-
-// const path = require('path')
-// const fs = require('fs')
-//const uploadPath = path.join('public', Book.coverImageBasePath)
-// const multer = require('multer')
-// const upload = multer({
-//     dest: uploadPath,
-//     fileFilter: (req, file, callback) => {
-//       callback(null, imageMimeTypes.includes(file.mimetype))
-//     }
-// })
+const Author = require('../models/author')
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -37,52 +26,120 @@ router.get('/', async (req, res) => {
     res.redirect('/')
   }
 })
-  
-  // New Book Route
+
+// New Book Route
 router.get('/new', async (req, res) => {
   renderNewPage(res, new Book())
 })
 
-//created BookS ROute
-router.post('/',  async (req, res) => {
-  //const fileName = req.file != null ? req.file.filename : null
+// Create Book Route
+router.post('/', async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
-    publishData: new Date(req.body.publishData),
+    publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    // coverImageName: fileName,
     description: req.body.description
   })
   saveCover(book, req.body.cover)
 
   try {
     const newBook = await book.save()
-    // res.redirect(`books/${newBook.id}`)
-    res.redirect(`books`)
+    res.redirect(`books/${newBook.id}`)
   } catch {
-    // if (book.coverImageName != null) {
-    //   removeBookCover(book.coverImageName)
-    // }
     renderNewPage(res, book, true)
   }
 })
 
-// function removeBookCover(fileName) {
-//   fs.unlink(path.join(uploadPath, fileName), err => {
-//     if (err) console.error(err)
-//   })
-// }
+// Show Book Route
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+                           .populate('author')
+                           .exec()
+    res.render('books/show', { book: book })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit Book Route
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+    renderEditPage(res, book)
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Update Book Route
+router.put('/:id', async (req, res) => {
+  let book
+
+  try {
+    book = await Book.findById(req.params.id)
+    book.title = req.body.title
+    book.author = req.body.author
+    book.publishDate = new Date(req.body.publishDate)
+    book.pageCount = req.body.pageCount
+    book.description = req.body.description
+    if (req.body.cover != null && req.body.cover !== '') {
+      saveCover(book, req.body.cover)
+    }
+    await book.save()
+    res.redirect(`/books/${book.id}`)
+  } catch {
+    if (book != null) {
+      renderEditPage(res, book, true)
+    } else {
+      redirect('/')
+    }
+  }
+})
+
+// Delete Book Page
+router.delete('/:id', async (req, res) => {
+  let book
+  try {
+    book = await Book.findById(req.params.id)
+    await book.remove()
+    res.redirect('/books')
+  } catch {
+    if (book != null) {
+      res.render('books/show', {
+        book: book,
+        errorMessage: 'Could not remove book'
+      })
+    } else {
+      res.redirect('/')
+    }
+  }
+})
 
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find({})
     const params = {
       authors: authors,
       book: book
     }
-    if (hasError) params.errorMessage = 'Error Creating Book'
-    res.render('books/new', params)
+    if (hasError) {
+      if (form === 'edit') {
+        params.errorMessage = 'Error Updating Book'
+      } else {
+        params.errorMessage = 'Error Creating Book'
+      }
+    }
+    res.render(`books/${form}`, params)
   } catch {
     res.redirect('/books')
   }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-
+  require('dotenv').config()
+}
 
 const express = require('express')
 const app = express()
@@ -10,7 +9,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/author')
+const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
@@ -19,7 +18,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -28,8 +27,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
-app.use('/author', authorRouter)
+app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
-
 
 app.listen(process.env.PORT || 3000)
